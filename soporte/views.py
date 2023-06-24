7dedeeffc02ba339623762652dbe3eaf41510606
alex-bq <ale.barrientos@duocuc.cl>
2023-06-24 00:26:25 -0400

    mmm q rica pera

--- soporte/views.py
+++ soporte/views.py
@@ -25,44 +25,10 @@ def eliminacionFeedback(request,id):
     feedbacks.delete()
     return redirect('/')
     
-
 def soporte(request):
-    if request.method is not "POST":
-        tipos= tipoMensaje.objects.all();
-        ciudades= ciudad.objects.all()
-        context={'tipo':tipos,'ciudad':ciudades}
-        return render(request,'soporte/Agregar.html',context)
-    else :
-        nombre=request.POST["nombre"]
-        correo=request.POST["correo"]
-        tipoMensaje=request.POST["tipoMensaje"]
-        tipoMensaje=request.POST["tipoMensaje"]
-        comentario=request.POST["comentario"]
-        activo="1"
-
-        objTipoMensaje=tipoMensaje.objects.get(id_tipo_mensaje = tipoMensaje)
-        obj=Usuario.objects.create ( nombre=nombre,
-                                    correo=correo,
-                                    tipoMensaje=tipoMensaje,
-                                    tipoMensaje=tipoMensaje,
-                                    comentario=comentario,
-                                    activo=1 )
-        obj.save()
-        context={'mensaje':"Ok, datos grabados..."}
-        return render (request, 'soporte/Agregar.html', context)
+    return render(request, "soporte.html")
 
 
-def registrarSoporte(request):
-    nombre=request.POST['txtNombre']
-    correo=request.POST['txtCorreo']
-    tipoMensa=request.POST['txtTipo']
-    ciudad=request.POST['txtCiudad']
-    mensaje=request.POST['txtMensaje']
-
-    mensajeSoporte=feedback.objects.create(
-        nombre=nombre, correo=correo, tipoMensa=tipoMensa, ciudad=ciudad, mensaje=mensaje
-    )
-    return redirect('/')
 
 
         
