af1017a296325c48416be81074282c3595db98eb
Perrogamer777 <lucho.man229@gmail.com>
2023-07-09 20:32:48 -0400

    Registro bonito

--- web/forms.py
+++ web/forms.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))   
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"})) 
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"})) 
+    email = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"})) 
+    password1 = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"})) 
+    password2 = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"})) 
     
     class Meta:
         model = User
